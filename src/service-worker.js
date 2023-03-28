95a6f4dafc7b90312db79aadf42f45794f997df7
Steinberg99 <stein@schaapontwerpers.nl>
2023-03-28 13:11:43 +0200

    Added service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,24 @@
-self.addEventListener("install", () => {
-  console.log("service-worker.js loaded");
+const RUNTIME_CACHE = "runtime-cache";
+const CORE_CACHE = "cache-v1";
+const CORE_ASSETS = ["/offline", "/index.css", "/index.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CORE_CACHE)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.headers.get("accept").includes("text/html")) {
+    event.respondWith(
+      caches
+        .open(RUNTIME_CACHE)
+        .then((cache) => cache.match(event.request))
+        .then((response) => response || fetch(event.request))
+        .catch(() => caches.open(CORE_CACHE).then((cache) => cache.match("/offline"))),
+    );
+  }
 });
